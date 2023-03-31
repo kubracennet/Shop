7c605c87e610c041ff25742e9e716e57c4550cdc
kubracennet <kubracennetyavasoglu@gmail.com>
2023-03-31 15:13:03 +0300

    Initial Commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Kübra Cennet Yavaşoğlu on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
